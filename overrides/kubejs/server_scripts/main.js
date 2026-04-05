cad72658462d039f1d39230d55cd9bb0b21d3d42
Simsblock <146435639+Simsblock@users.noreply.github.com>
2026-04-06 01:28:12 +0200

    Crafting Overhauls

--- overrides/kubejs/server_scripts/main.js
+++ overrides/kubejs/server_scripts/main.js
@@ -20,22 +20,51 @@ ServerEvents.tags('item', event => {
         'upgrade_aquatic:lionfish',
         'upgrade_aquatic:perch',
         'upgrade_aquatic:pike',
+        'pastel:koi'
     ])
     event.add('c:large_fish', [
         '#hybrid-aquatic:large_fish',
     ])
     event.add('c:foods/cooked_fish', 'hybrid-aquatic:cooked_fish_meat')
     event.add('c:foods/cooked_fish', 'hybrid-aquatic:cooked_fish_steak')
+    event.add('c:pots', [
+        'minersdelight:copper_pot',
+        'farmersdelight:cooking_pot',
+        'youkaisfeasts:steamer_pot',
+        'youkaisfeasts:small_iron_pot',
+        'youkaisfeasts:short_iron_pot',
+        'youkaisfeasts:stockpot',
+    ])
+    event.add('c:trident_materials', [
+        'hybrid-aquatic:shark_tooth',
+        'upgrade_aquatic:thrasher_tooth',
+        'create_aquatic_ambitions:spiky_shell',
+    ])
 })
 
 ServerEvents.recipes(event => {
 
+    event.remove({ id: 'minecraft:recipes/diamond_sword' })
     event.remove({ id: 'farmersdelight:cooking/crab_cakes' })
+    event.remove({ id: 'farmersdelight:cooking/safety_net' })
     event.remove({ id: 'hybrid-aquatic:recipes/raw_crab' })
     event.remove({ id: 'hybrid-aquatic:recipes/raw_fish_meat_small' })
     event.remove({ id: 'hybrid-aquatic:recipes/raw_fish_meat_medium' })
     event.remove({ id: 'hybrid-aquatic:recipes/raw_fish_steak' })
+    event.remove({ id: 'hybrid-aquatic:recipes/trident' })
+    event.remove({ id: 'upgrade-aquatic:recipe/trident' })
+    event.remove({ id: 'create-aquatic-ambitions:recipe/crafting/materials/trident' })
     event.remove({ id: 'youkaishomecoming:recipes/crab_roe' })
+    event.remove({ id: 'beautify:recipes/hanging_pot' })
+    event.remove({ id: 'dustydecorations:recipes/rope' })
+    event.remove({ id: 'dustydecorations:recipes/wedged_knife' })
+    event.remove({ id: 'dustydecorations:recipes/wedged_cleaver' })
+    event.remove({ id: 'dustydecorations:recipes/pots_and_pans_stack' })
+
+    event.remove({ output: 'create_sa:rose_quartz_sword' })
+    event.remove({ output: 'create_sa:rose_quartz_axe' })
+    event.remove({ output: 'create_sa:rose_quartz_pickaxe' })
+    event.remove({ output: 'create_sa:rose_quartz_shovel' })
 
     event.custom({
         type: "farmersdelight:cooking",
@@ -95,4 +124,134 @@ ServerEvents.recipes(event => {
         '#c:crabs'
     ])
 
+    event.shaped('3x supplementaries:rope', [
+        ' S ',
+        ' S ',
+        '   '
+    ], {
+        S: 'armersdelight:straw',
+    })
+
+    event.shapeless('4x supplementaries:rope', [
+        'farmersdelight:safety_net'
+    ])
+
+    event.shaped('beautify:hanging_pot', [
+        ' S ',
+        ' P ',
+        '   '
+    ], {
+        S: 'supplementaries:rope',
+        P: 'minecraft:flower_pot'
+    })
+
+    event.shaped('farmersdelight:safety_net', [
+        'SS ',
+        'SS ',
+        '   '
+    ], {
+        S: 'supplementaries:rope',
+    })
+
+    event.shaped('3x dustydecorations:rope', [
+        ' S ',
+        ' S ',
+        ' S '
+    ], {
+        S: 'supplementaries:rope',
+    })
+
+    event.shapeless('dustydecorations:wedged_knife', [
+        'farmersdelight:iron_knife',
+    ])
+
+    event.shapeless('dustydecorations:wedged_cleaver', [
+        'dungeonsdelight:iron_cleaver'
+    ])
+
+    event.shapeless('3x dustydecorations:pots_and_pans', [
+        '3x #c:pots'
+    ])
+
+    event.shaped('minecraft:trident', [
+        ' MM',
+        ' SM',
+        'S  '
+    ], {
+        S: 'create_aquatic_ambitions:prismarine_alloy_rod',
+        M: '#c:trident_materials'
+    })
+
+    event.shaped('minecraft:diamond_sword', [
+        ' D ',
+        ' D ',
+        ' S '
+    ], {
+        D: 'minecraft:diamond',
+        S: 'create_sa:zinc_handle'
+    })
+
+    event.smithing(
+        'create_sa:rose_quartz_sword', 
+        'minecraft:air',                
+        'minecraft:golden_sword', 
+        'create:polished_rose_quartz'
+    )
+
+    event.smithing(
+        'create_sa:rose_quartz_axe', 
+        'minecraft:air',                
+        'minecraft:golden_axe', 
+        'create:polished_rose_quartz'
+    )
+
+    event.smithing(
+        'create_sa:rose_quartz_pickaxe', 
+        'minecraft:air',                
+        'minecraft:golden_pickaxe', 
+        'create:polished_rose_quartz'
+    )
+
+    event.smithing(
+        'create_sa:rose_quartz_shovel', 
+        'minecraft:air',                
+        'minecraft:golden_shovel', 
+        'create:polished_rose_quartz'
+    )
+
+    event.shaped('minecraft:chainmail_helmet', [
+        'INI',
+        'N N',
+        '   '
+    ], {
+        I: 'minecraft:iron_ingot',
+        N: 'minecraft:iron_nugget'
+    })
+
+    event.shaped('minecraft:chainmail_chestplate', [
+        'N N',
+        'INI',
+        'NIN'
+    ], {
+        I: 'minecraft:iron_ingot',
+        N: 'minecraft:iron_nugget'
+    })
+
+    event.shaped('minecraft:chainmail_leggings', [
+        'NIN',
+        'I I',
+        'N N'
+    ], {
+        I: 'minecraft:iron_ingot',
+        N: 'minecraft:iron_nugget'
+    })
+
+    event.shaped('minecraft:chainmail_boots', [
+        '  ',
+        'N N',
+        'I I'
+    ], {
+        I: 'minecraft:iron_ingot',
+        N: 'minecraft:iron_nugget'
+    })
 })
