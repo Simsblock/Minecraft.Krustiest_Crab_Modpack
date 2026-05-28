82db30734aa1436d360828058f4905dfe88596d7
Simsblock <146435639+Simsblock@users.noreply.github.com>
2026-05-28 19:38:21 +0200

    diverse fixes

--- overrides/kubejs/client_scripts/oef-fix.js
+++ overrides/kubejs/client_scripts/oef-fix.js
@@ -1,27 +1,14 @@
+// kubejs/client_scripts/recipe_viewer_unification.js
+// Fluid removal doesnt work atm :<
+
+const REMOVED_BUCKETS = [
+    'tfmg:hydrogen_bucket',
+    'tfmg:molten_slag_bucket',
+    'createbigcannons:molten_steel_bucket'
+];
+
 RecipeViewerEvents.removeEntriesCompletely('item', event => {
-    
-    const itemsToRemove = [
-		"createmetallurgy:molten_steel_bucket",
-        "createmetallurgy:molten_slag_bucket",
-        "createbb:hydrogen_bucket",
-        "tfmg:molten_steel_bucket",
-    ];
-
-    itemsToRemove.forEach(item => {
-        event.remove(item);
-    });
-
-});
-
-RecipeViewerEvents.removeEntriesCompletely('fluid', event => {
-	const fluidsToRemove = [
-        "createmetallurgy:molten_slag",
-        "createbb:hydrogen",
-        "createmetallurgy:molten_steel",
-        "tfmg:molten_steel",
-    ];
-	
-	fluidsToRemove.forEach(fluid => {
-        event.remove(Fluid.of(fluid));
+    REMOVED_BUCKETS.forEach(bucket => {
+        event.remove(bucket);
     });
 });
